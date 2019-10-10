2efe83c6b79e37628740f7f7cda007ed8a10ba94
holistudio <daniel.bin.lu@gmail.com>
2019-10-10 15:15:44 -0400

    tested numpy

--- Assignment_1/application.py
+++ Assignment_1/application.py
@@ -1,2 +1,14 @@
 import numpy as np
 from numpy import linalg as LA
+import os
+
+
+def main():
+    currentDir=os.getcwd();
+    print(f"Current Directory: {currentDir}");
+    m = np.matrix('1 2 3; 4 5 6; 7 8 9');
+    print(f"Matrix {m}");
+
+
+if __name__ == "__main__":
+    main()
