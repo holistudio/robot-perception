b59b291c768f4b17b8a2f78446ab79f4c2d08521
holistudio <daniel.bin.lu@gmail.com>
2019-10-11 14:14:47 -0400

    Reorganized Task 1 code to separate folder

---
+++ Assignment_1/task_1/task_1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2 as cv
+import matplotlib as mpl
+from numpy import linalg as LA
+from matplotlib import pyplot as plt
+from PIL import Image
+import os
+
+
+def main():
+    currentDir=os.getcwd();
+
+    img = cv.imread('data/for_watson.png',0);
+
+    # display image in grayscale
+    # cv.imshow('image',img);
+    # cv.waitKey(0);
+    # cv.destroyAllWindows();
+
+    # seeing the image in grayscale, a very faint message appears!
+
+    # convert pixels to array
+    pixels = np.array(img);
+
+    # plot pixels and record rows where message appears
+    # plt.imshow(pixels, cmap='gray', vmin=0, vmax=255);
+    # plt.show()
+
+    # check rows 75, 250, and 390
+    # print(pixels[75,:]); #background is 29, message is 30
+    # print(pixels[240,:]); #background is 149, message is 150
+    # print(pixels[390,:]); #background is 76, message is 77
+
+    # change pixels to black and white based on color values from rows
+    for i in range(0,len(pixels)):
+        for j in range(0,len(pixels[i,:])):
+            if (pixels[i,j] == 29 or pixels[i,j] == 149 or pixels[i,j] == 76):
+                pixels[i,j]=0;
+            if (pixels[i,j] == 30 or pixels[i,j] == 150 or pixels[i,j] == 77):
+                pixels[i,j]=255;
+
+    # plot new image
+    plt.imshow(pixels, cmap='gray', vmin=0, vmax=255);
+    plt.show();
+
+if __name__ == "__main__":
+    main()
