b91c3d66b0dd7025c839233f4b44dbd7dc42b263
holistudio <daniel.bin.lu@gmail.com>
2019-10-21 21:22:06 -0400

    initial open3d code and RANSAC pseudocode for Task 5

--- Assignment_1/application.py
+++ Assignment_1/application.py
@@ -1,35 +1,33 @@
 import numpy as np
 from math import sqrt
+import open3d as o3d
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, z):
+        self.x = x;
+        self.y = y;
+        self.z = z;
 
 def main():
-    # array of vanishing point coordinates
-    v = [Point(1911.622,-2888.174), Point(6577.018,3963.804), Point(-1821.64,4067.708)];
+    # load point cloud data points
+    pcd = o3d.io.read_point_cloud("data/record_00348.pcd");
+    print(pcd);
+    print(np.asarray(pcd.points))
+    o3d.visualization.draw_geometries([pcd])
 
-    # center point coordinates
-    c = Point(1965.774,1488.922);
 
-    # based on derivations for focal length:
-    f1 = sqrt(-(v[0].x-c.x)*(v[1].x-c.x)-(v[0].y-c.y)*(v[1].y-c.y));
-    print(f"Focal Length, f= {f1} (points 1 and 2)");
-
-    # use the other two pairs of vanishing points to calculate focal length
-    f2 = sqrt(-(v[1].x-c.x)*(v[2].x-c.x)-(v[1].y-c.y)*(v[2].y-c.y));
-    f3 = sqrt(-(v[0].x-c.x)*(v[2].x-c.x)-(v[0].y-c.y)*(v[2].y-c.y));
-
-    # as expected the three pairs yield the same value
-    print(f"Focal Length, f= {f2} (points 2 and 3)");
-    print(f"Focal Length, f= {f3} (points 1 and 3)");
-
-    #final calibration method.
-    K = np.matrix([[f1, 0, c.x],[0, f1, c.y],[0, 0, 1]]);
-
-    print(f"\nCalibration Matrix:\n{K}")
-
+    # set consensus score threshold to 80% of number of data points
+    # for N trials
+        # randomly select s data points and build a plane as the model
+        # determine data points within distance threshold, t, of model
+        # assign number of data points as consensus score of that model
+        # if model score is less than score threshold, T
+            # repeat this process
+        # else
+            # build a new plane with the entire consensus set of data points/inliers
+        # store plane model and its consensus score
+    # at the end of trials
+    # display plane with the highest consensus score
 
 if __name__ == "__main__":
     main()
